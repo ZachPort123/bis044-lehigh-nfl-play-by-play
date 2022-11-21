7411024425ead93ab7e44d15bd67784ab0dbe66c
Zachary Porter <zachport123@comcast.net>
2022-11-21 09:18:48 -0500

    Filtered for run and pass plays; added winning or losing column

--- question-1.R
+++ question-1.R
@@ -1,1 +1,8 @@
+pbp_2021 <- load_pbp(2021)
+run_pass_plays <- pbp_2021 %>%
+  filter(play_type == "run" | play_type == "pass") %>%
+  mutate(winning_or_losing = if_else(score_differential < 0, "losing", if_else(score_differential > 0, "winning", "tied")))
 
+
+
+table(run_pass_plays$play_type)
