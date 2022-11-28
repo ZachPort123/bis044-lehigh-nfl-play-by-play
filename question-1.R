5a89535b206ad7f178aa572edf157d8bf533b607
Zachary Porter <zachport123@comcast.net>
2022-11-28 08:55:33 -0500

    Created table

--- question-1.R
+++ question-1.R
@@ -1,8 +1,17 @@
+library(nflfastR)
+library(ggplot2)
+library(dplyr)
 pbp_2021 <- load_pbp(2021)
 run_pass_plays <- pbp_2021 %>%
   filter(play_type == "run" | play_type == "pass") %>%
-  mutate(winning_or_losing = if_else(score_differential < 0, "losing", if_else(score_differential > 0, "winning", "tied")))
+  mutate(winning_or_losing = if_else(score_differential < 0, "losing", if_else(score_differential > 0, "winning", "tied"))) %>%
+  select(play_type, winning_or_losing)
 
+run_pass_plays_total <- run_pass_plays %>%
+  group_by(winning_or_losing) %>%
+  summarise(plays = n())
 
+table <- table(run_pass_plays)
+table
 
-table(run_pass_plays$play_type)
+barplot(table, beside=True)
