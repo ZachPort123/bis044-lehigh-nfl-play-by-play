ba3ecf5e18467dd4d3fadc5ae507b98f2692b239
Zachary Porter <zachport123@comcast.net>
2022-12-06 14:01:53 -0500

    Finished Question 2
    Added scatter plot

--- question-2.R
+++ question-2.R
@@ -14,9 +14,17 @@ run_pass_third_downs <- pbp_2021 %>%
 # Finding conversion rates for rush and pass plays by yards to go
 conversion_rates <- run_pass_third_downs %>%
   group_by(play_type, ydstogo) %>%
-  mutate(conversion_rate = mean(first_down)) %>%
-  select(play_type, ydstogo, conversion_rate)
+  mutate(conversion_rate = mean(first_down), plays = n()) %>%
+  select(play_type, ydstogo, conversion_rate, plays)
 
-
-
-ggplot_shiny(conversion_rates)
+# Creating scatter plot relating yards to go to conversion rate for pass and run plays
+graph <- ggplot(conversion_rates, aes(x = ydstogo, y = conversion_rate, color = play_type)) +
+  geom_point() +
+  labs(title = "3rd Down Conversion Rates by Distance",
+       subtitle = "2021 NFL Season",
+       caption = "source: nflfastr",
+       x = "Yards To Go",
+       y = "Conversion Rate",
+       color = "Play Type") +
+  theme_bw()
+graph
