2d2ecaffab1d1920d1b39c35a67f3b9e6b35e8c6
Delfina Szigethy <djs525@lehigh.edu>
2022-12-05 19:32:26 -0500

    delfina's finished question 5

--- question-5.R
+++ question-5.R
@@ -1,2 +1,14 @@
-
 #delfina's question
+library(nflfastR)
+library(ggplot2)
+library(dplyr)
+pbp_2021 <- load_pbp(2021)
+run_plays <- pbp_2021 %>%
+  filter(play_type == "run")
+df_summary <- run_plays %>%
+  group_by(posteam) %>%
+  summarise(avg_yard=mean(yards_gained, na.rm=TRUE))
+
+
+
+#we want to use to posteam variable to group the data, use yards_gained for the summary 
